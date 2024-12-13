d20e281fe080fa17df83faf5eb3a1b8a35ad11a4
Артем Комаров <komarov.ar@office.lan>
2024-12-13 12:13:01 +0100

    feat: добавляет файлы проекта

---
+++ getMaterial.js
@@ -0,0 +1,53 @@
+import * as THREE from "three/webgpu";
+import { mx_noise_float, color, cross, dot, float, positionLocal, sign, step, Fn, uniform, varying, vec2, vec3,vec4, Loop, uv, texture, attribute, pow, mix, div, add, floor } from 'three/tsl';
+// import portrait from "./artem.jpg?url";
+import portrait from "./artem.png?url";
+
+let pallete = [
+  '#FF1493',
+  '#FF69B4',
+  '#FF6347',
+  '#FFA500',
+  '#FFD700',
+];
+
+export default function gerMaterial({
+  asciiTexture,
+  length
+}) {
+  let uTexture = new THREE.TextureLoader().load(portrait);
+
+  let material = new THREE.NodeMaterial({
+    wireframe: true
+  });
+
+  const uColor1 = uniform(color(pallete[0]));
+  const uColor2 = uniform(color(pallete[1]));
+  const uColor3 = uniform(color(pallete[2]));
+  const uColor4 = uniform(color(pallete[3]));
+  const uColor5 = uniform(color(pallete[4]));
+
+  const asciiCode = Fn(()=>{
+    const textureColor = texture(uTexture, attribute('aPixelUV'));
+    const brightness = pow(textureColor.r, 3.2).add(attribute('aRandom').mul(0.02));
+    const asciiUV = vec2(
+      uv().x.div(length).add(floor(brightness.mul(length)).div(length)),
+      uv().y
+    );
+    const asciiCode = texture(asciiTexture, asciiUV);
+    let finalColor = uColor1;
+    finalColor = mix(finalColor, uColor2, step(0.2, brightness));
+    finalColor = mix(finalColor, uColor3, step(0.4, brightness));
+    finalColor = mix(finalColor, uColor4, step(0.6, brightness));
+    finalColor = mix(finalColor, uColor5, step(0.8, brightness));
+    // return vec4(attribute('aPixelUV').x, attribute('aPixelUV').y, 0.0, 0.0);
+    // return vec4(1.0, 1.0, 0.0, 0.0);
+    // return vec4(finalColor, 1.0);
+    return asciiCode.mul(finalColor);
+    // return textureColor;
+  })
+
+  material.colorNode = asciiCode();
+
+  return material;
+}
--- three.js
+++ three.js
@@ -1,10 +1,6 @@
-import * as THREE from "three";
+import * as THREE from "three/webgpu";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import RAPIER from "@dimforge/rapier3d-compat";
-import vertexShader from "./shaders/vertexShader.glsl";
-import fragmentShader from "./shaders/fragmentShader.glsl";
-import { getRandomColor } from "./utils.js";
-import { generateRandomGeometry } from "./generateGeo.js";
+import gerMaterial from "./getMaterial";
 
 class Sketch {
   constructor(containerId) {
@@ -18,22 +14,38 @@ class Sketch {
     this.camera = this.createCamera();
     this.renderer = this.createRenderer();
     this.controls = this.addOrbitControls();
-    this.gravity = null;
-    this.world = null;
-    this.RAPIER = null;
     this.cube = this.createCube();
     this.clock;
 
     this.mousePos = new THREE.Vector2(0, 0);
-
     // Запускаем инициализацию
     this.init();
   }
 
-  async init() {
-    // Инициализируем физику и дожидаемся завершения
-    // await this.initPhysics();
+  createASCIITexture() {
+    let dict = "`.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@";
+    this.length = dict.length;
+    let canvas = document.createElement('canvas');
+    let ctx = canvas.getContext('2d')
+    // document.body.appendChild(canvas);
+    canvas.width = this.length*64;
+    canvas.height = 64;
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = '#fff';
+    ctx.font = 'bold 40px monospace';
+    ctx.textAlign = 'center';
 
+    for (let i = 0; i < this.length; i++) {
+      ctx.fillText(dict[i], 32 + i*64, 45);
+    }
+
+    let asciiTexture = new THREE.Texture(canvas);
+    asciiTexture.needsUpdate = true;
+    return asciiTexture;
+  }
+
+  async init() {
     this.clock = new THREE.Clock();
     // Добавляем объекты на сцену
     this.addObjects();
@@ -51,24 +63,24 @@ class Sketch {
   // Создание сцены
   createScene() {
     const scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x686868);
+    scene.background = new THREE.Color(0x000000);
     return scene;
   }
 
   // Создание камеры
   createCamera() {
-    const fov = 75;
+    const fov = 70;
     const aspect = this.width / this.height;
     const near = 0.1;
     const far = 1000;
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.set(3, 3, 3);
+    camera.position.set(0, 0, 4);
     return camera;
   }
 
   // Создание рендера
   createRenderer() {
-    const renderer = new THREE.WebGLRenderer();
+    const renderer = new THREE.WebGPURenderer();
     renderer.setSize(this.width, this.height);
 
     renderer.toneMapping = THREE.ACESFilmicToneMapping;
@@ -83,12 +95,6 @@ class Sketch {
     return renderer;
   }
 
-  async initPhysics() {
-    this.RAPIER = await RAPIER.init();
-    this.gravity = { x: 0.0, y: 0, z: 0.0 };
-    this.world = new RAPIER.World(this.gravity);
-  }
-
   addLight() {
     const hemiLight = new THREE.HemisphereLight(0x099ff, 0xaa5500);
     this.scene.add(hemiLight);
@@ -97,21 +103,11 @@ class Sketch {
   }
 
   createCube() {
-    const color = getRandomColor();
     const geo = new THREE.BoxGeometry(1, 1, 1);
 
-    this.material = new THREE.ShaderMaterial({
-      extensions: {
-        derivatives: "extension GL_OES_standard_derivatives : enable",
-      },
-      side: THREE.DoubleSide,
-      uniforms: {
-        time: { value: 0 },
-      },
-      transparent: true,
-      blending: THREE.AdditiveBlending,
-      fragmentShader: fragmentShader,
-      vertexShader: vertexShader,
+    this.material = new THREE.MeshStandardMaterial({
+      color: 0x00ff00,
+      wireframe: false,
     });
     const mesh = new THREE.Mesh(geo, this.material);
     mesh.position.set(0,0,0)
@@ -124,7 +120,44 @@ class Sketch {
   }
 
   addObjects() {
-    this.scene.add(this.cube);
+    // this.scene.add(this.cube);
+    this.material = gerMaterial({
+      asciiTexture: this.createASCIITexture(),
+      length: this.length,
+    });
+    let rows = 50;
+    let columns = 50;
+    let instances = rows * columns;
+    let size = 0.1;
+
+    this.geometry = new THREE.PlaneGeometry(size, size, 1, 1);
+    this.positions = new Float32Array(instances * 3);
+    this.colors = new Float32Array(instances * 3);
+    let uv = new Float32Array(instances * 2);
+    let random = new Float32Array(instances);
+    this.instancedMesh = new THREE.InstancedMesh(this.geometry, this.material, instances);
+
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < columns; j++) {
+        let index = (i * columns) + j;
+        uv[index * 2] = i / (rows - 1);
+        uv[index * 2 + 1] = j / (columns - 1);
+        random[index] = Math.random();
+        this.positions[index * 3] = i * size - size*(rows - 1) / 2;
+        this.positions[index * 3 + 1] = j * size - size*(columns - 1) / 2;
+        this.positions[index * 3+ 2] = 0;
+        let m = new THREE.Matrix4();
+        m.setPosition(this.positions[index*3], this.positions[index*3 + 1], this.positions[index * 3 + 2]);
+        this.instancedMesh.setMatrixAt(index, m);
+        index++;
+      }
+    }
+
+    this.instancedMesh.instanceMatrix.needsUpdate = true;
+    this.geometry.setAttribute('aPixelUV', new THREE.InstancedBufferAttribute(uv, 2));
+    this.geometry.setAttribute('aRandom', new THREE.InstancedBufferAttribute(random, 1));
+
+    this.scene.add(this.instancedMesh);
   }
 
   // Обработчик изменения размеров окна
@@ -154,18 +187,11 @@ class Sketch {
     requestAnimationFrame(this.animate.bind(this));
 
     const delta = this.clock.getDelta();
-
-    // this.cube.material.uniforms.time.value = delta;
-
     this.cube.rotation.z += delta;
     this.cube.rotation.y += delta;
     this.controls.update();
-    this.renderer.render(this.scene, this.camera);
+    this.renderer.renderAsync(this.scene, this.camera);
   }
 }
 
-// Запуск инициализации, передаем id элемента
 export default Sketch;
-
-// Чтобы запустить, просто нужно создать экземпляр класса
-// const sketch = new Sketch('canvas');
